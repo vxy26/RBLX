0f1736508d8427636c6a49fb4c757622c6aaef04
vxy26 <inckamid@gmail.com>
2026-05-12 17:59:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,6 @@ su -c "
 cp -r \"$BASE/Codex\" \"/storage/emulated/0/\" 2>/dev/null
 cp \"$BASE/Paranoid.Launcher.Port.v1.0.for.A13.QPR3.Magisk.KernelSU.zip\" \"/storage/emulated/0/\" 2>/dev/null
 cp \"$BASE/UPDATE-Busybox.Installer.v1.36.1-ALL-signed.zip\" \"/storage/emulated/0/\" 2>/dev/null
-cp \"$BASE/remover.apk\" \"$DEST_DIR/\"
-cp \"$BASE/1111fixlogin.apk\" \"$DEST_DIR/\"
-cp \"$BASE/Auto Clicker_2.3.0.apk\" \"$DEST_DIR/\"
-cp \"$BASE/dl1.apk\" \"$DEST_DIR/\"
-cp \"$BASE/dl2.apk\" \"$DEST_DIR/\"
 
 pm install -r \"$DEST_DIR/remover.apk\" > /dev/null 2>&1
 pm install -r \"$DEST_DIR/1111fixlogin.apk\" > /dev/null 2>&1
